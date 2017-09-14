334f7bf1a9a0e8d89024e837b1036567bd096e46
mikesaprykin <mikesaprykin@gmail.com>
2017-09-14 07:28:41 +0300

    feat(styling): Added proper layout and styling for the project

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,7 +4,7 @@ const logo = require('./logo.png');
 export const Logo = () => {
     return (
         <div className="Logo">
-            <img src={logo} />
+            <img src={logo} alt="Iscalfive Logo"/>
         </div>
     )
 };
--- src/containers/app/App.tsx
+++ src/containers/app/App.tsx
@@ -3,14 +3,17 @@ import { Logo } from '../../components';
 import { LoginForm } from '../login/Login';
 import './App.css';
 
-// const notebook = require('./notebook.png');
+const notebook = require('./notebook.png');
 
 export class App extends React.Component {
   render() {
     return (
       <div className="App">
+          <img className="app-notebook-image" src={notebook} alt="Notebook"/>
           <div className="app-login">
-            <Logo />
+              <div className="app-login-logo">
+                <Logo />
+              </div>
             <LoginForm />
           </div>
       </div>
